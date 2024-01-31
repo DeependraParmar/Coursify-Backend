c03b333d126254587e6b2684849d78338f5b1c9c
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-31 12:13:08 +0530

    Added the courses to get my profile

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,86 +1,64 @@
+import cloudinary from "cloudinary";
+import crypto from "crypto";
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
-import passport from "passport";
-import { sendToken } from "../utils/sendToken.js";
+import { Course } from "../models/Course.js";
+import { InstructorStats } from "../models/InstructorStats.js";
+import { Review } from "../models/Review.js";
 import User from "../models/User.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
 import getDataUri from "../utils/dataUri.js";
-import cloudinary from "cloudinary";
-import crypto from "crypto";
 import { sendEmail } from "../utils/sendMail.js";
-import { Review } from "../models/Review.js";
-import { Course } from "../models/Course.js";
-import { InstructorStats } from "../models/InstructorStats.js";
+import { sendToken } from "../utils/sendToken.js";
 
 // controller to register a user 
-export const register = catchAsyncError( async(req,res,next) => {
-    const {name,email,password} = req.body;
+export const register = catchAsyncError(async (req, res, next) => {
+    const { name, email, password } = req.body;
 
-    if( !name || !email || !password)
-        return (next(new ErrorHandler("All the fields are required",400)));
+    if (!name || !email || !password)
+        return (next(new ErrorHandler("All the fields are required", 400)));
 
-    let user = await User.findOne({email});
+    let user = await User.findOne({ email });
 
-    if(user) 
-        return next(new ErrorHandler("User Already Exists. Please go to Login",409));
+    if (user)
+        return next(new ErrorHandler("User Already Exists. Please go to Login", 409));
 
 
     user = await User.create({
-        name,email,password,
+        name, email, password,
         avatar: {
             public_id: "default",
             url: "https://asset.cloudinary.com/dmmrtqe8q/bd8f69b303633e784950d90b22da09a6"
         }
     });
 
-    sendToken(res,user,"User Registered Successfully",201);
+    sendToken(res, user, "User Registered Successfully", 201);
 });
 
 
 
 // controller to login a  user 
-export const login = catchAsyncError(async(req,res,next)=> {
-    const {email,password} = req.body;
+export const login = catchAsyncError(async (req, res, next) => {
+    const { email, password } = req.body;
 
-    if( !email || !password){
+    if (!email || !password) {
         return next(new ErrorHandler("All Fields are Required", 400));
     }
 
-    const user = await User.findOne({email}).select("+password");
+    const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
+    if (!user) {
         return next(new ErrorHandler("User Doesn't Exist. Please Register First", 401));
     }
 
     const isMatched = await user.comparePassword(password);
 
-    if(!isMatched)
-        return next(new ErrorHandler("Incorrect Email or Password",401));
+    if (!isMatched)
+        return next(new ErrorHandler("Incorrect Email or Password", 401));
 
 
-    sendToken(res,user,`Welcome back, ${user.name}`,200)
+    sendToken(res, user, `Welcome back, ${user.name}`, 200)
 });
 
-// ********************* CONTROLLER FOR GOOGLE AUTHENTICATION ********************
-// controller for the google authentication at /api/v1/auth/google
-export const googleAuthController = (passport.authenticate("google", {
-    scope: ["profile", "email"],
-}));
-
-// google login callback controller for the route /api/v1/auth/google/callback
-export const googleAuthCallbackController = (passport.authenticate("google", {
-    failureRedirect: "/api/v1/auth/google/error",
-    successRedirect: "/api/v1/auth/google/success"
-}));
-
-// controller for the /success route 
-export const googleAuthSuccessController = (passport.authenticate("google", {}), async(req,res,next) => {
-    res.send("<h1>Google Authentication Successfull</h1> <br> <a href='/api/v1/profile'>Go to profile</a> <br> <a href='/api/v1/logout'>Logout</a>")
-});
-
-// controller for the /error route 
-export const googleAuthErrorController = catchAsyncError(async (req,res,next) => {
-    res.send("<h1>Google Authentication Failed</h1> <br> <a href='/'>Go to home</a> <br> <a href='/auth/google'>Login with Google</a>")
-});
 
 
 // getting the profile of the user
@@ -92,12 +70,18 @@ export const myProfile = catchAsyncError(async (req, res, next) => {
 });
 
 // getting the public profile of thè user 
-export const getPublicProfile = catchAsyncError( async(req,res,next) => {
-    const {id} = req.params;
+export const getPublicProfile = catchAsyncError(async (req, res, next) => {
+    const { id } = req.params;
     const dbUser = await User.findById(id);
 
-    if(!dbUser){
+    if (!dbUser) {
         return next(new ErrorHandler("User not found!", 404));
+    }
+
+    let createdCourses = [];
+
+    if(dbUser.isVerifiedInstructor){
+        createdCourses = await Course.find({createdBy: dbUser._id});
     }
 
     res.status(200).json({
@@ -106,25 +90,26 @@ export const getPublicProfile = catchAsyncError( async(req,res,next) => {
             name: dbUser.name,
             email: dbUser.email,
             avatar: dbUser.avatar,
+            courses: createdCourses,
         }
     })
 })
 
 // updating the profile details of the user: name,email and about
-export const updateProfile = catchAsyncError( async(req,res,next) => {
-    const {name,email,about,phoneNumber} = req.body;
+export const updateProfile = catchAsyncError(async (req, res, next) => {
+    const { name, email, about, phoneNumber } = req.body;
     const user = await User.findById(req.user._id);
 
-    if(name){
+    if (name) {
         user.name = name;
     }
-    if(email){
+    if (email) {
         user.email = email;
     }
-    if(about){
+    if (about) {
         user.about = about;
     }
-    if(phoneNumber){
+    if (phoneNumber) {
         user.phoneNumber = phoneNumber;
     }
 
@@ -163,17 +148,17 @@ export const updateProfilePicture = catchAsyncError(async (req, res, send) => {
 
 // changing the password of the user
 export const changePassword = catchAsyncError(async (req, res, next) => {
-    const {oldPassword, newPassword} = req.body;
+    const { oldPassword, newPassword } = req.body;
     const user = await User.findById(req.user._id).select("+password");
 
-    if(!user){
+    if (!user) {
         return next(new ErrorHandler("User Doesn't Exist. Please Register First", 401));
     }
 
     const isMatched = await user.comparePassword(oldPassword);
 
-    if(!isMatched)
-        return next(new ErrorHandler("Incorrect Old Password",401));
+    if (!isMatched)
+        return next(new ErrorHandler("Incorrect Old Password", 401));
 
     user.password = newPassword;
     await user.save();
@@ -186,17 +171,17 @@ export const changePassword = catchAsyncError(async (req, res, next) => {
 
 // controller for forgot password
 // controller for forget password 
-export const forgetPassword = catchAsyncError( async(req,res,next) => {
-    const {email} = req.body;
+export const forgetPassword = catchAsyncError(async (req, res, next) => {
+    const { email } = req.body;
 
-    if(!email){
-        return next(new ErrorHandler("Email is Required",400));
+    if (!email) {
+        return next(new ErrorHandler("Email is Required", 400));
     }
-    
-    const user = await User.findOne({email});
 
-    if(!user){
-        return next(new ErrorHandler("User not found with this email",404));
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return next(new ErrorHandler("User not found with this email", 404));
     }
 
     const resetToken = await user.getResetToken();
@@ -207,7 +192,7 @@ export const forgetPassword = catchAsyncError( async(req,res,next) => {
     const message = `Click on the link to reset your password: ${url}. If your have not requested then, please ignore.`
 
     // send token via email 
-    await sendEmail(user.email,"Coursify: Forgot Password!! No Worries. Reset it",message);
+    await sendEmail(user.email, "Coursify: Forgot Password!! No Worries. Reset it", message);
 
     res.status(200).json({
         success: true,
@@ -217,8 +202,8 @@ export const forgetPassword = catchAsyncError( async(req,res,next) => {
 
 // resetting the user password from tokenisation 
 // controller for reset password 
-export const resetPassword = catchAsyncError( async(req,res,next) => {
-    const {token} = req.params;
+export const resetPassword = catchAsyncError(async (req, res, next) => {
+    const { token } = req.params;
 
     const resetPasswordToken = crypto.createHash('sha256').update(token).digest('hex');
 
@@ -229,8 +214,8 @@ export const resetPassword = catchAsyncError( async(req,res,next) => {
         }
     });
 
-    if(!user){
-        return next(new ErrorHandler("Token is invalid or been expired",401));
+    if (!user) {
+        return next(new ErrorHandler("Token is invalid or been expired", 401));
     }
 
     user.password = req.body.password;
@@ -294,13 +279,13 @@ export const getMyCourses = catchAsyncError(async (req, res, next) => {
 
 
 // get all the courses created by the user
-export const getCreatedCourses = catchAsyncError(async(req,res,next) => {
+export const getCreatedCourses = catchAsyncError(async (req, res, next) => {
     const user = await User.findById(req.user._id);
     const userId = user._id;
 
-    const myCourses = await Course.find({createdBy: userId});
-    
-    if(!myCourses){
+    const myCourses = await Course.find({ createdBy: userId });
+
+    if (!myCourses) {
         return next(new ErrorHandler("No Courses created yet", 400));
     }
 
@@ -325,26 +310,26 @@ export const logout = catchAsyncError((req, res, next) => {
 });
 
 // controller for getting the instructor stats
-User.watch().on("change", async (req,res,next) => {
+User.watch().on("change", async (req, res, next) => {
     const stats = [];
-    try{
+    try {
         const instructor = await User.findById(req.user._id);
-        if(!instructor){
+        if (!instructor) {
             return new ErrorHandler("Instructor not found", 404);
         }
         stats = await InstructorStats.find({ instructorId: instructor._id }).sort({ createdAt: "desc" }).limit(1);
     }
-    catch(error){
+    catch (error) {
         return new ErrorHandler("Error getting instructor stats", 500);
     }
 
     const statsData = stats[0].metrics;
 
     const courses = await Course.find({ createdBy: req.user._id });
-    statsData.totalCourses = await Course.countDocuments({createdBy: req.user._id});
+    statsData.totalCourses = await Course.countDocuments({ createdBy: req.user._id });
     statsData.totalStudentsEnrolled = await Course.find({ createdBy: req.user._id }).countDocuments();
     statsData.totalEarnings = courses.reduce((total, course) => total + (course.totalPurchases * course.price), 0);
     statsData.createdAt = new Date(Date.now());
- 
+
     await stats[0].save();
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,9 @@
 import express from "express";
-import { changePassword, forgetPassword, getCreatedCourses, getMyCourses, getPublicProfile, googleAuthCallbackController, googleAuthController, googleAuthErrorController, googleAuthSuccessController, login, logout, myProfile, register, registerAsInstructor, resetPassword, updateProfile, updateProfilePicture } from "../controllers/userControllers.js";
+import { changePassword, forgetPassword, getCreatedCourses, getMyCourses, getPublicProfile, login, logout, myProfile, register, registerAsInstructor, resetPassword, updateProfile, updateProfilePicture } from "../controllers/userControllers.js";
 import { isAuthenticated, isVerifiedInstructor } from "../middlewares/auth.js";
 import singleUpload from "../middlewares/multer.js";
 
 const router = express.Router();
-
-
-// *************** ROUTES FOR GOOGLE AUTHENTICATION *******************************
-// creating the route for google authentication
-router.get("/auth/google", googleAuthController);
-
-// creating a route for the login button and the logout button 
-router.get("/auth/google/callback", googleAuthCallbackController);
-
-// creating a route for the successfull google authentication 
-router.get("/auth/google/success", googleAuthSuccessController);
-
-// creating a route for error while google authentication 
-router.get("/auth/google/error", googleAuthErrorController);
 
 
 
@@ -28,7 +14,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", isAuthenticated, myProfile);
 router.post("/updateprofile", isAuthenticated, updateProfile);
-router.put("/updateprofilepicture", isAuthenticated,singleUpload, updateProfilePicture);
+router.put("/updateprofilepicture", isAuthenticated, singleUpload, updateProfilePicture);
 router.route("/changepassword").put(isAuthenticated, changePassword);
 router.route("/forgotpassword").post(forgetPassword);
 router.route("/resetpassword/:token").put(resetPassword);
@@ -37,7 +23,7 @@ router.route("/profile/public/:id").get(getPublicProfile);
 
 
 // routes for instructor
-router.post("/be-an-instructor",isAuthenticated,singleUpload,registerAsInstructor);
+router.post("/be-an-instructor", isAuthenticated, singleUpload, registerAsInstructor);
 router.get("/instructor/my-courses", isAuthenticated, isVerifiedInstructor, getCreatedCourses);
 
 
