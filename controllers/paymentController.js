8fdf31d349e3d419b43699516dec79a59d15d4e6
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-03-08 13:24:27 +0530

    Payment Model accessed successfully for deployment

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -19,30 +19,30 @@ export const checkOut = async(req,res,next) => {
 }
 
 export const paymentVerification = async (req, res, next) => {
-    // const {razorpay_order_id, razorpay_payment_id, razorpay_signature} = req.body;
+    const {razorpay_order_id, razorpay_payment_id, razorpay_signature} = req.body;
     const user = await User.findById(req.user._id);
     const course = await Course.findById(req.params.course_id);
     
-    // const body = razorpay_order_id + "|" + razorpay_payment_id;
-    // const expectedSignature = crypto.createHmac('sha256', process.env.RAZORPAY_API_SECRET).update(body.toString()).digest('hex')
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto.createHmac('sha256', process.env.RAZORPAY_API_SECRET).update(body.toString()).digest('hex')
 
-    // const isAuthenticSignature = expectedSignature === razorpay_signature;
+    const isAuthenticSignature = expectedSignature === razorpay_signature;
 
-    // if(isAuthenticSignature){
-    //     // save all the things to the database 
-    //     await Payment.create({
-    //         razorpay_order_id,razorpay_payment_id,razorpay_signature
-    //     });
-    //     user.courses.push({
-    //         course: course._id,
-    //         thumbnail: course.poster.public_id,
-    //     });
-    //     await user.save();
-    //     res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`)
-    // }
-    // else{
-    //     res.redirect(`${process.env.FRONTEND_URL}/paymentfailed?reference=${razorpay_payment_id}`)
-    // }
+    if(isAuthenticSignature){
+        // save all the things to the database 
+        await Payment.create({
+            razorpay_order_id,razorpay_payment_id,razorpay_signature
+        });
+        user.courses.push({
+            course: course._id,
+            thumbnail: course.poster.public_id,
+        });
+        await user.save();
+        res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`)
+    }
+    else{
+        res.redirect(`${process.env.FRONTEND_URL}/paymentfailed?reference=${razorpay_payment_id}`)
+    }
 
     user.courses.push({
         course: course._id,
