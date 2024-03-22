4276550736d4180c2da37949d8cd51a9be220a41
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-03-22 07:54:45 +0530

    Updated the Payment schema and added emoji to invoice

--- models/Payment.js
+++ models/Payment.js
@@ -12,7 +12,46 @@ const schema = new mongoose.Schema({
     razorpay_signature: {
         type: String,
         required: true
+    },
+    transaction_date: {
+        type: Date,
+        required: true,
+        default: new Date(Date.now()).toDateString(),
+    },
+    invoice_no: {
+        type: String,
+        required: true,
+        unique: true,
+        default: `INV-${Date.now()}`
+    },
+    user: {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        }
+    },
+    transaction_amount: {
+        type: Number,
+        required: true
+    },
+    course: {
+        name: {
+            type: String,
+            required: true
+        },
+        creator: {
+            type: String,
+            required: true
+        }
     }
 });
 
-export const Payment = mongoose.model("Payments",schema);
+export const Payment = mongoose.model("Payments", schema);
