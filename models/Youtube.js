0d5fb93990127ab4f7b98638346e06be4e254d0f
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-28 10:38:42 +0530

    Youtube Free Course Model done

---
+++ models/Youtube.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    title: String,
+    description: String,
+    poster: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    },
+    lectures: [
+        {
+            title: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+            },
+            description: {
+                type: String,
+                required: true,
+            },
+        }
+    ]
+});
+
+export default mongoose.model("Youtube", schema);
