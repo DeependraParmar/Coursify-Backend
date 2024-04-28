b7db167b7e8bd9de625864b14227293df1a0302a
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-28 10:47:42 +0530

    Edit free course done

--- controllers/youtubeControllers.js
+++ controllers/youtubeControllers.js
@@ -40,4 +40,52 @@ export const createFreeCourse = catchAsyncError( async(req, res, next) => {
         success: true,
         message: "Course created successfully",
     });
-})
+});
+
+export const editFreeCourse = catchAsyncError( async(req, res, next) => {
+    const { title, description} = req.body;
+    const file = req.file;
+    const course = await Youtube.findById(req.params.id);
+
+    if(!course)
+        return next(new ErrorHandler("Course not found", 404));
+
+    if(title)
+        course.title = title;
+
+    if(description)
+        course.description = description;
+
+    if(file){
+        await cloudinary.v2.uploader.destroy(course.poster.public_id);
+        const dataUri = getDataUri(file);
+        const cloud = await cloudinary.v2.uploader.upload(dataUri.content);
+
+        course.poster = {
+            public_id: cloud.public_id,
+            url: cloud.secure_url,
+        };
+    }
+    
+    await course.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Course updated successfully",
+    });
+});
+
+export const deleteFreeCourse = catchAsyncError( async(req, res, next) => {
+    const course = await Youtube.findById(req.params.id);
+
+    if(!course)
+        return next(new ErrorHandler("Course not found", 404));
+
+    await cloudinary.v2.uploader.destroy(course.poster.public_id);
+    await course.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Course deleted successfully",
+    });
+});
--- routes/youtubeRoutes.js
+++ routes/youtubeRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createFreeCourse, getFreeCourses } from "../controllers/youtubeControllers.js";
+import { createFreeCourse, editFreeCourse, getFreeCourses } from "../controllers/youtubeControllers.js";
 import { isAuthenticated, isVerifiedAdmin } from "../middlewares/auth.js";
 import singleUpload from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.route('/free-course').get(getFreeCourses).post(isAuthenticated, isVerifiedAdmin, singleUpload, createFreeCourse);
+
+router.route('/free-course/:id').put(isAuthenticated, isVerifiedAdmin, singleUpload, editFreeCourse);
 
 
 
